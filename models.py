5b707bcbbb54f84540569187e071152ba62a0312
Karly Stark <karlystark@gmail.com>
2023-07-26 12:16:16 -0700

    added User model

--- models.py
+++ models.py
@@ -1,1 +1,26 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "user"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True)
+
+    first_name = db.Column(
+        db.String(30),
+        nullable=False)
+
+    last_name = db.Column(
+        db.String(50),
+        nullable=False)
+
+    image_url = db.Column(
+        db.String,
+        nullable=False,
+        default='https://is3-ssl.mzstatic.com/image/thumb/Purple30/v4/89/a6/70/89a67093-f892-cdcd-00af-76d6a68c4ec0/source/256x256bb.jpg'
+    )
+
