9bec85d5d9f75859dca4ce03c7ebff2e89b706b1
jesse.chung9727@gmail.com <jesse.chung9727@gmail.com>
2023-07-26 15:31:14 -0400

    creating flask app

--- models.py
+++ models.py
@@ -2,6 +2,13 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
+def connect_db(app):
+    """Connect to database."""
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
 class User(db.Model):
     __tablename__ = "user"
 
