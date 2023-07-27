95649e32a1547f163aefd29d2af1b2be002fab30
Karly Stark <karlystark@gmail.com>
2023-07-26 17:18:11 -0700

    completed routes and html for user profile and edit pages

--- app.py
+++ app.py
@@ -3,6 +3,7 @@
 import os
 
 from flask import Flask, redirect, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, User
 
 
@@ -20,22 +21,28 @@ debug = DebugToolbarExtension(app)
 
 @app.get('/')
 def home_page():
-
+    """Redirect to user list page"""
     return redirect('/users')
 
 
 @app.get('/users')
 def users():
+    """Renders user list page"""
     users = User.query.all()
+
     return render_template('userlist.html', users = users)
+
 
 @app.get('/users/new')
 def new_users_form():
-
+    """Renders new user form"""
     return render_template('newusersform.html')
+
 
 @app.post('/users/new')
 def process_new_user():
+    """Collects new user form data and creates a new User instance, updating
+    the database. Redirects to users list page."""
     first_name = request.form['firstname']
     last_name = request.form['lastname']
     image_url = request.form['imageurl']
@@ -49,3 +56,64 @@ def process_new_user():
 
     return redirect('/users')
 
+
+@app.get('/users/<int:user_id>')
+def show_user_profile(user_id):
+    """Collects user data and renders user profile page."""
+    user = User.query.get(user_id)
+
+    first_name = user.first_name
+    last_name = user.last_name
+    image_url = user.image_url
+
+    return render_template('userprofile.html',
+                           first_name = first_name,
+                           last_name = last_name,
+                           image_url = image_url)
+
+
+@app.get('/users/<int:user_id>/edit')
+def show_user_edit_page(user_id):
+    """Collects user data and renders user profile edit page."""
+    user = User.query.get(user_id)
+
+    first_name = user.first_name
+    last_name = user.last_name
+    image_url = user.image_url
+
+    return render_template('usereditpage.html',
+                           first_name = first_name,
+                           last_name = last_name,
+                           image_url = image_url)
+
+
+@app.post('/users/<int:user_id>/edit')
+def process_user_edits(user_id):
+    """Collect user edit form data and edit user instance in database,
+    redirect to user list page."""
+    user = User.query.get(user_id)
+
+    first_name = request.form['firstname']
+    last_name = request.form['lastname']
+    image_url = request.form['imageurl']
+
+    user.first_name = first_name
+    user.last_name = last_name
+    user.image_url = image_url
+
+    db.session.commit()
+
+    return redirect('/users')
+
+
+@app.post('/users/<int:user_id>/edit')
+def delete_user_profile(user_id):
+    """Deletes user from database on delete button click, redirects to
+    user list page."""
+    user = User.query.get(user_id)
+
+    db.session.delete(user)
+
+    db.session.commit()
+
+    return redirect('/users')
